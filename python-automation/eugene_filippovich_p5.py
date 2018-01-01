7fc6822b0ae254f104287ba95b961a52e876d757
Eugene Filippovich <eugene.filippovich93@gmail.com>
2018-01-01 21:13:12 +0300

    Working list #1

--- python-automation/eugene_filippovich_p5.py
+++ python-automation/eugene_filippovich_p5.py
@@ -1,3 +1,14 @@
-import
-if else
-    create  df
+import os
+import pprint
+
+def current_files():
+    files_with_path = []
+    path = "F:/Test"
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            f = os.path.join(root, f)
+            files_with_path.append(f)
+    pprint.pprint(files_with_path)
+    return current_files
+
+current_files()
