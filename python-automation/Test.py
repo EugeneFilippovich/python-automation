a710058b184a8c042745a238c310b629f47a2644
Eugene Filippovich <eugene.filippovich93@gmail.com>
2018-01-08 09:54:17 +0300

    First

--- python-automation/Test.py
+++ python-automation/Test.py
@@ -1,81 +1,21 @@
-import random
+def censor(forbidden, substitution):
+    def wrapper(func):
+        def inner_wrapper():
+            res = func()
+            for word in forbidden:
+                res = res.replace(word, substitution)
+            return res
+        return inner_wrapper
+    return wrapper
 
 
-def arange(*args, **kwargs):
-    if len(args) == False and len(kwargs) == False:
-        print("Input at least one Argument or Key Argument")
-        return
-
-    elif len(args) + len(kwargs) > 3:
-        print("Input max 3 arguments")
-        return
-
-    elif len(args):
-        if(args[0] * args[1]) < 0 or (args[0] * args[2]) < 0 or (args[1] * args[2]) < 0 or args[2] == 0:
-            print("You have negative or zero stepping. Infinite loop causes.")
-            return
-        elif len(args) == 1:
-            start = 0
-            stop = args[0]
-            step = 1
-
-        elif len(args) == 2:
-            start = args[0]
-            stop = args[1]
-            step = 1
-
-        elif len(args) == 3:
-            start = args[0]
-            stop = args[1]
-            step = args[2]
-            if step > stop:
-                print("Error! Your 'step' argument is greater than 'stop' argument")
-                return
-
-        while start <= stop:
-            yield start
-            start += step
-
-    elif len(kwargs):
-        # HELP! if I use less than 3 kwargs - error occurs
-        if (int(kwargs['start']) * int(kwargs['stop'])) < 0 or (int(kwargs['start']) * int(kwargs['step'])) < 0 or (int(kwargs['stop']) * int(kwargs['step'])) < 0 or int(kwargs['step'] == 0):
-            print("You have negative or zero stepping. Infinite loop causes.")
-            return
-
-        elif "stop" in kwargs and len(kwargs) == 1:
-            start = 0
-            stop = int(kwargs['stop'])
-            step = 1
-
-        elif "start" and "stop" in kwargs and len(kwargs) == 2:
-            start = kwargs[0]
-            stop = kwargs[1]
-            step = 1
-
-        elif "stop" and "step" in kwargs and len(kwargs) == 2:
-            start = 0
-            stop = kwargs[1]
-            step = kwargs [2]
-        elif "start" and "stop" and "step" in kwargs and len(kwargs) == 3:
-            start = int(kwargs["start"])
-            stop = int(kwargs["stop"])
-            step = int(kwargs["step"])
-
-        while start <= stop:
-            yield start
-            start += step
+@censor(forbidden=("ipsum", "quis"), substitution="[CENSORED]")
+def censored():
+    return """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+        deserunt mollit anim id est laborum."""
 
 
-# var = int(raw_input("0 - manual, 1 - args, 2 - kwargs: "))
-
-# if var == 0:
-for n in arange(5):
-    print(n)
-#
-# elif var == 1:
-#
-#     for n in arange(int(input("Input start: ")), int(input("Input stop: ")) , int(input("Input step: "))):
-#         print(n)
-# elif var == 2:
-#     for n in arange(start=int(input("Input start: ")), stop=1000000000000000000000000000000000000000000000000000000000000000000, step=random.randint(12345678912345, 123456789123456789123456789)):
-#         print(n)
+print(censored())
---
+++ python-automation/eugene_filippovich_p7.py
@@ -0,0 +1,44 @@
+class CarProduce(object):
+    car_id = 1
+
+    def __init__(self):
+        print("Creating new car {}".format(id(self)))
+        self.car_id = CarProduce.car_id
+        CarProduce.car_id += 1
+        self.start_price = 10000
+        self.gas_tank = 60
+
+
+
+class Petrol(CarProduce):
+    def __init__(self):
+        super(Petrol, self).__init__()
+        self.petrol_price = 2.4
+        self.mileage_before_repair = 100000
+        self.repair_cost = 500
+        self.fuel_consumption = 8
+
+
+class Diesel(CarProduce):
+    def __init__(self):
+        super(Diesel, self).__init__()
+        self.diesel_price = 1.8
+        self.mileage_before_repair = 150000
+        self.repair_cost = 750
+        self.fuel_consumption = 6
+
+[Petrol() for x in xrange(1, 100, 2)]
+
+
+# range()
+
+
+# [Petrol() for x in xrange(1, 100, 10)]
+
+
+
+# car1 = Diesel()
+
+# print(car1.__dict__)
+
+
