0a6a54a43d6cdb8470cf0f5178697cee47b15393
user <eugene.filippovich93@gmail.com>
2018-01-16 17:15:36 +0300

    Second shitty

--- python-automation/eugene_filippovich_p8.py
+++ python-automation/eugene_filippovich_p8.py
@@ -1,30 +1,37 @@
 class Store(object):
     pass
-passed = ['Banana', 'Apple']
+
+passed = ['Banana', 'Apple', 'Pizza']
 
 grocery_items_list = []
+
 hardware_items_list = []
+
+
 class GroceryStore(Store):
+    goods_list = []
+
     def add_item(self, item):
-        if item in passed:
-            grocery_items_list.append(item)
-        else:
-            raise TypeError("Incorrect item")
+        if isinstance(item, Food):
+            self.goods_list.append(item)
 
     def add_items(self, *items):
-        grocery_items_list.extend(items)
+        self.goods_list.extend(items)
 
     def delete_item(self, item):
-        if item in grocery_items_list:
-            grocery_items_list.remove(item)
+        if item in self.goods_list:
+            self.goods_list.remove(item)
         else:
             raise TypeError("Incorrect item")
 
     def delete_items(self, *items):
-        grocery_items_list = [c for c in items if c != items]
-        return grocery_items_list
+        for item in items:
+            if item in self.goods_list:
+                self.goods_list.remove(item)
 
-
+    def info(self):
+        for good in self.goods_list:
+            print (good.__dict__)
 
 
 class HardwareStore(Store):
@@ -32,6 +39,8 @@ class HardwareStore(Store):
 
 
 class Goods(object):
+    freezed = False
+
     def __init__(self, price):
         # self.discount_percentage = discount_percentage
         if price > 0:
@@ -43,16 +52,21 @@ class Goods(object):
     def _price(self):
         return self.price
 
-    def freeze_price(self, boolean, value):
-        self.boolean = boolean
-        if self.boolean == False:
-            self.price = value
+    def set_price(self, price):
+        if self.freezed == False:
+            self.price = price
         else:
-            raise ValueError("No access for price changing")
+            raise Exception("Price can't be changed")
+
+    def freeze_price(self, is_freezed):
+        self.freezed = is_freezed
 
     def set_discount(self, discount_percentage):
-        self.discount_percentage = discount_percentage
-        print("Discount percentage is : {}% ".format(discount_percentage))
+        if self.freezed == False:
+            self.discount_percentage = discount_percentage
+            print("Discount percentage is : {}% ".format(discount_percentage))
+        else:
+            raise Exception("Discount can't be applied")
     #
 
     def reset_discount(self):
@@ -60,49 +74,78 @@ class Goods(object):
         self.discount_percentage = 0
 
 
-
-
-
-
-
 class Food(Goods):
     pass
 
 
 class Pizza(Food):
+    def __init__(self, price):
+        self.name = 'Pizza'
+        super(Pizza, self).__init__(price)
     pass
 
 
 class Cheese(Food):
+    def __init__(self, price):
+        self.name = 'Cheese'
+        super(Cheese, self).__init__(price)
     pass
 
 
 class Bread(Food):
+    def __init__(self, price):
+        self.name = 'Cheese'
+        super(Bread, self).__init__(price)
     pass
+
 
 
 class Avocado(Food):
+    def __init__(self, price):
+        self.name = 'Cheese'
+        super(Avocado, self).__init__(price)
     pass
+
 
 
 class Tools(Goods):
+    def __init__(self, price):
+        self.name = 'Cheese'
+        super(Tools, self).__init__(price)
     pass
+
 
 
 class Bow(Tools):
+    def __init__(self, price):
+        self.name = 'Cheese'
+        super(Bow, self).__init__(price)
     pass
+
 
 
 class Shield(Tools):
+    def __init__(self, price):
+        self.name = 'Cheese'
+        super(Shield, self).__init__(price)
     pass
+
 
 
 class Stuff(Tools):
+    def __init__(self, price):
+        self.name = 'Cheese'
+        super(Stuff, self).__init__(price)
     pass
+
 
 
 class Wheel(Tools):
+    def __init__(self, price):
+        self.name = 'Cheese'
+        super(Wheel, self).__init__(price)
     pass
+
 
 # belmarket = GroceryStore()
 # bananas = Banana(6)  # create a banana with 6$ price
@@ -122,12 +165,17 @@ class Wheel(Tools):
 b = Goods(44)
 b.set_discount(4)
 b.reset_discount()
-b.freeze_price(False, 42)
+b.freeze_price(False)
+b.set_price(42)
 
 s = GroceryStore()
-s.add_item('Apple')
-s.add_items('args', 'asdfsdf', 'ag')
-s.delete_items('args', 'ag')
+p = Pizza(6)
+p1 = Cheese(8)
+s.add_items(p, p1)
+s.delete_items(p, p1)
+
+# print (p.__dict__)
+
 
 # belmarket = Goods(42, 4)
 #
@@ -138,4 +186,4 @@ s.delete_items('args', 'ag')
 # discount = bananas.discount_percentage(4)
 # print (s.__dict__)
 
-print grocery_items_list
+s.info()
