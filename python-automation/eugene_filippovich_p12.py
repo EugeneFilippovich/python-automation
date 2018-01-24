42790223e0418fa688a01fe49b2278097c310332
user <eugene.filippovich93@gmail.com>
2018-01-24 23:36:17 +0300

    First shitty

--- python-automation/eugene_filippovich_p12.py
+++ python-automation/eugene_filippovich_p12.py
@@ -1,13 +1,21 @@
-import os
-from getpass import getpass
 import logging
 import paramiko
+import os
 
+USERNAME = 'eugene'
 logging.basicConfig(level=logging.INFO)
+
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-file = open('E:\Config_file.txt', 'r')
+file = open('/home/eugene/Config_file', 'r')
+ssh.connect('localhost', username='eugene', password='4356')
+ssh.exec_command('mkdir -p ~/.ssh/')
+ssh.exec_command('echo "%s" >> ~/.ssh/authorized_keys')
+ssh.exec_command('chmod 600 ~/.ssh/authorized_keys')
+ssh.exec_command('chmod 700 ~/.ssh/')
+ssh.exec_command('ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""')
+ssh.close()
 
 for line in file:
     info = {}
@@ -31,10 +39,12 @@ for line in file:
     ssh.exec_command('chmod 600 ~/.ssh/authorized_keys')
     ssh.exec_command('chmod 700 ~/.ssh/')
     ssh.exec_command('ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P ""')
+    ssh.exec_command('cat ~/.ssh/id_rsa.pub | ssh root@192.168.56.103 %22cat >> ~/.ssh/authorized_keys%22')
 
-    ftp_client = ssh.open_sftp()
-    ftp_client.get('~./ssh/authorized_keys', "E:/Bash/authorized_keys", 'rw')
-    ftp_client.close()
+
+    # ftp_client = ssh.open_sftp()
+    # ftp_client.get('~./ssh/authorized_keys', "E:/Bash/authorized_keys", 'rw')
+    # ftp_client.close()
     # key = open(os.path.expanduser('~/.ssh/id_rsa.pub')).read() #need to seek for remote machine
     # username = os.getlogin()
     # password = getpass()
@@ -43,6 +53,16 @@ for line in file:
     # stdin, stdout, stderr = ssh.exec_command("ls /etc/")
     # for line in stdout.readlines():
     #     print line.strip()
+
     ssh.close()
+
+# ssh.exec_command('cat ~/.ssh/id_rsa.pub | ssh root@192.168.56.101 %22cat >> ~/.ssh/authorized_keys%22')
+
+
+# for line in file:
+#     info = {}
+#     info['ip'] = line.split(' ')[0]
+#
+#     ssh.exec_command('ssh-copy id $USERNAME@')
 
 
