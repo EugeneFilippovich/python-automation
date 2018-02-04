7fd81dca97188ca7430ade222052408e45ba5cfa
Eugene Filippovich <eugene.filippovich93@gmail.com>
2018-02-04 20:48:17 +0300

    First attempt

--- python-automation/home_tasks/eugene_filippovich_p10.py
+++ python-automation/home_tasks/eugene_filippovich_p10.py
@@ -1,14 +1,16 @@
 from operator import attrgetter
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
 class Bouquet(object):
     def __init__(self):
         self.flower_bouquet = []
         self.total_price = 0
         self.average_life_time = 0.0
 
-    def __getitem__(self, price):
-        return getattr(self, price)
+    # def __getitem__(self, price):
+    #     return getattr(self, price)
 
     def add_flowers(self, *flowers):
             for flower in flowers:
@@ -36,6 +38,10 @@ class Bouquet(object):
     def check_contains(self, flower):
         return flower in self.flower_bouquet
 
+    # object is not iterable
+    # def search_by_key(self):
+    #     for flower in self.flower_bouquet:
+    #         filter(lambda price: price['price'] == 30, flower)
 
 class Flower(object):
 
@@ -69,23 +75,23 @@ r = Roses('Red', 40, 5, 3)
 t = Tulips('White', 50, 4, 4)
 p = Peonies('Purple', 30, 2, 3)
 bouquet.add_flowers(r, t, p)
-print(bouquet.get_total_price())
-print(bouquet.get_average_life_time())
+logging.info(bouquet.get_total_price())
+logging.info(bouquet.get_average_life_time())
 
-bouquet.destruct_bouquet()
-
-print(bouquet.check_contains(p))
+# bouquet.destruct_bouquet()
+# logging.info(bouquet.search_by_key())
+logging.info(bouquet.check_contains(p))
 
 b2 = Bouquet()
 p2 = Peonies('Purple', 30, 2, 3)
 b2.add_flowers(r, p2)
-# bouquet.sorting_by_key()
-# print(bouquet.life_time)
+bouquet.sorting_by_key()
+# logging.info(bouquet.life_time)
 
 for i in bouquet.flower_bouquet:
-    print i.__dict__
+    logging.info(i.__dict__)
 
 print ('____________')
 
 for i in b2.flower_bouquet:
-    print i.__dict__
+    logging.info(i.__dict__)
